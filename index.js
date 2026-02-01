fa995aaed7e4da24626fbc5e33fb5b7d9095c992
vinaykumar C <vinaykumarc204@gmail.com>
2026-02-01 22:50:48 +0530

    All Files Are Being Added

---
+++ index.js
@@ -0,0 +1,16 @@
+const noBtn = document.querySelector(".btn2"); // I changed this from 'btn2' to 'noBtn'
+const container = document.querySelector(".lowerContainer");
+
+noBtn.addEventListener("mouseenter", () => {
+    const containerRect = container.getBoundingClientRect();
+    const btnRect = noBtn.getBoundingClientRect();
+
+    const maxX = containerRect.width - btnRect.width;
+    const maxY = containerRect.height - btnRect.height;
+
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+
+    noBtn.style.left = `${randomX}px`;
+    noBtn.style.top = `${randomY}px`;
+});
